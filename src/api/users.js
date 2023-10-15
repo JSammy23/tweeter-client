e785b9843e131114830a62ebfdca45118b412af6
Jordan <jordansampson23@gmail.com>
2023-10-15 14:54:22 -0500

    API index & users module created.

---
+++ src/api/users.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'http://localhost:3000/users';
+
+// Create new user
+export const createUser = async (credentials) => {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+    return data;
+};
