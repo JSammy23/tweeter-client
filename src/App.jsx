0508994b05faa4a7b212e4474bc6c1034dd52cbb
Jordan <jordansampson23@gmail.com>
2023-10-15 15:48:36 -0500

    Add Routes & BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import SignUpPage from "./pages/SignUp/SignUpPage";
 
 function App() {
   
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
     </>
   )
 }
