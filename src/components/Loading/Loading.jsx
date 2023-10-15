923b7cb5417b0001db84d87a9560eec36f74225d
Jordan <jordansampson23@gmail.com>
2023-10-15 15:40:03 -0500

    Create loading page & styles.

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,38 @@
+import './Loading.styles.css';
+
+const Loading = () => {
+  return (
+    <div className="overlay">
+        <div className="background">
+            <div className="boxes">
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Loading
