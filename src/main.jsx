8059072d78749cc15995c25ba4d652ee10fdfa44
Jordan <jordansampson23@gmail.com>
2023-10-15 16:08:00 -0500

    Add themeProvider for styled components.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import {  BrowserRouter } from 'react-router-dom';
+import theme from './styles/theme.js';
+import { ThemeProvider } from 'styled-components';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
