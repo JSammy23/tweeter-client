61899569044067fe83209b16ba0c1563c48cad14
Jordan <jordansampson23@gmail.com>
2023-10-15 15:39:18 -0500

    Add loginUser function.

--- src/api/users.js
+++ src/api/users.js
@@ -18,3 +18,23 @@ export const createUser = async (credentials) => {
     const data = await response.json();
     return data;
 };
+
+// Login
+export const loginUser = async (credentials) => {
+    const response = await fetch(`${BASE_URL}/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    })
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+    localStorage.setItem('token', data.token);
+    return data;
+};
