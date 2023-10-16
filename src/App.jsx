f66140e21f5b9cf64dd9b0c3483ea1024f17699b
Jordan <jordansampson23@gmail.com>
2023-10-16 01:51:49 -0500

    Add FeedPage & private Routes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUpPage from "./pages/SignUp/SignUpPage";
+import PrivateRoute from "./components/PrivateRoute";
+import FeedPage from "./pages/Feed/FeedPage";
+import HomeContent from "./components/HomeContent";
 
 function App() {
   
@@ -12,6 +15,14 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/*" element={<PrivateRoute>
+          <FeedPage>
+            <Routes>
+              <Route path="/home" element={<HomeContent />} />
+            </Routes>
+          </FeedPage>
+        </PrivateRoute> } >
+        </Route>
       </Routes>
     </>
   )
---
+++ src/pages/Feed/FeedPage.jsx
@@ -0,0 +1,93 @@
+// import { useContext, useEffect } from 'react'
+import Sidebar from '../../components/Sidebar';
+
+
+import styled from 'styled-components';
+import { Background, Wrapper } from '../../styles/styledComponents';
+// import { AppContext } from 'services/appContext';
+// import Loading from 'components/Loading/Loading';
+
+
+const Flex = styled.div`
+  display: flex;
+  
+  @media (max-width: 683px) {
+    flex-direction: column;
+  }
+`;
+
+const FeedContainer = styled.div`
+ width: 100%;
+ height: 100vh;
+ max-width: 683px;
+ /* grid-column: 2 / 3; */
+ flex: 3;
+ flex-shrink: 1;
+ order: 1;
+ overflow-y: scroll;
+ border-right: 1px solid;
+ border-left: 1px solid;
+ border-color: ${props => props.theme.colors.secondary};
+
+ @media (min-width: 838px) {
+  min-width: 683px;
+ }
+
+ @media (max-width: 683px) {
+  padding-bottom: 3.5em;
+  width: 100vw;
+  overflow-y: auto;
+ }
+
+ @media (min-width: 683px) and (max-width: 838px) {
+    /* flex-shrink: 0; */
+    min-width: 600px;
+  }
+
+ /* Hide the scrollbar */
+ &::-webkit-scrollbar {
+    width: 0em;
+    background-color: transparent;
+  }
+
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background-color: transparent;
+  }
+
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  /* Hide the scrollbar in Mozilla Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: transparent transparent;
+
+  /* Track */
+  scrollbar-track-color: transparent;
+
+  /* Handle */
+  scrollbar-thumb-color: transparent;
+`;
+
+const FeedPage = ({ children }) => {
+  
+
+  // if (loading) {
+  //   return <Loading/>
+  // }
+  
+  return (
+    <Background>
+      <Wrapper>
+        <Flex>
+        <Sidebar/>
+        <FeedContainer>{children}</FeedContainer>
+        </Flex>
+      </Wrapper>
+    </Background>
+  )
+}
+
+export default FeedPage
