cfb971cf579ded808a3fd33f3c15221b795c3d15
Jordan <jordansampson23@gmail.com>
2023-10-18 11:46:16 -0500

    Add UserProvider.

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import App from './App.jsx';
 import {  BrowserRouter } from 'react-router-dom';
 import theme from './styles/theme.js';
 import { ThemeProvider } from 'styled-components';
+import { UserProvider } from './services/userContext.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
