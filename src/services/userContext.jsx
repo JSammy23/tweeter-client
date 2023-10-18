665b89d712a6252e607d5b710326b4b96b7f955c
Jordan <jordansampson23@gmail.com>
2023-10-18 11:46:01 -0500

    intial user context added to store currentUser.

---
+++ src/services/userContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const contextValue = {
+        currentUser,
+        setCurrentUser
+    }
+
+    return (
+        <UserContext.Provider value={contextValue} >
+            {children}
+        </UserContext.Provider>
+    )
+};
