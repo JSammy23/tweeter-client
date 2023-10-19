ba15be32d3f8dcdc4f3424f64fc258c87909e0ff
Jordan <jordansampson23@gmail.com>
2023-10-19 14:20:42 -0500

    Add compose btn, handleComposeTweet fn.

--- src/components/Compose.jsx
+++ src/components/Compose.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 
 import { TweetCard } from '../styles/tweetStyles';
+import { Button } from '../styles/styledComponents';
 import styled from 'styled-components';
+import { composeTweet } from '../api/tweets';
 
 const StyledTextArea = styled.textarea`
     width: 100%;
-    padding: .3em 1.5em;
+    padding: .3em .5em;
     font-size: 1.3em;
     font-family: 'Times New Roman', Times, serif;
     border: none;
@@ -38,10 +40,23 @@ const Compose = () => {
         setEditorState(e.target.value);
     };
 
+    const handleComposeTweet = async () => {
+        const body = {
+            text: editorState
+        }
+        try {
+            const newTweet = await composeTweet(body);
+            console.log('Tweet composed successfully!');
+            setEditorState('');
+        } catch (error) {
+            console.error('Error composing tweet:', error)
+        }
+    };
+
 
   return (
     <TweetCard>
-        <div>UserImage</div>
+        {/* User profile pic */}
         <ComposeBody>
             <StyledTextArea
             maxLength='500'
@@ -49,7 +64,9 @@ const Compose = () => {
             rows="5"
             value={editorState}
             onChange={handleInputChange} />
-            <Controls>Tweet</Controls>
+            <Controls>
+                <Button onClick={handleComposeTweet}>Tweet</Button>
+            </Controls>
         </ComposeBody>
         
     </TweetCard>
