95842b60e2bba05997f0a206927af8d6cf3de750
Jordan <jordansampson23@gmail.com>
2023-10-19 14:20:13 -0500

    Compose Tweet API fn added.

--- src/api/tweets.js
+++ src/api/tweets.js
@@ -1,5 +1,8 @@
 const BASE_URL = 'http://localhost:3000/tweets';
 
+/****** Fetch Tweets ********/
+
+// Fetch Tweets for Home content
 export const fetchHomeTweets = async () => {
     const token = localStorage.getItem('token');
     const response = await fetch(`${BASE_URL}/home`, {
@@ -18,4 +21,28 @@ export const fetchHomeTweets = async () => {
     const tweets = await response.json();
     console.log(tweets);
     return tweets;
+};
+
+/******** Create Tweets ********/
+
+// Compose Tweet
+export const composeTweet = async (body) => {
+    const token = localStorage.getItem('token');
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `${token}`
+        },
+        body: JSON.stringify(body),
+    })
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
 };
