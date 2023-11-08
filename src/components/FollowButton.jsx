aae1efe1091d8e1828234f8dc9698f571a4239d5
Jordan <jordansampson23@gmail.com>
2023-11-08 13:07:46 -0600

    Add FollowButton Component.

---
+++ src/components/FollowButton.jsx
@@ -0,0 +1,74 @@
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../services/userContext';
+import { followUser } from '../api/users';
+
+import styled from 'styled-components';
+import { Button } from '../styles/styledComponents';
+import { faCheck } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+const Checkmark = styled.span`
+ position: relative;
+ margin-left: .3em;
+`;
+
+const FollowButton = ({ user }) => {
+    const [isFollowing, setIsFollowing] = useState(false);
+    const { currentUser } = useContext(UserContext);
+  
+    useEffect(() => {
+      checkIsFollowing();
+    }, [currentUser, user]);
+  
+    const checkIsFollowing = () => {
+      const isFollowing = currentUser.following.some((followingUser) => followingUser === user);
+      setIsFollowing(isFollowing);
+    };
+
+    const handleFollow = async () => {
+      if (isFollowing) {
+        try {
+          setIsFollowing(false);
+          await followUser(user);
+        } catch (error) {
+          console.error('Error unfollowing user', error);
+        }
+      } else {
+        try {
+          setIsFollowing(true);
+          await followUser(user);
+        } catch (error) {
+          console.error('Error following user', error);
+        }
+      }
+    };
+
+    const renderFollowButton = () => {
+        if (isFollowing) {
+            return (
+                <Button onClick={handleFollow} >
+                    Following
+                    <Checkmark>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </Checkmark>
+                </Button>
+            )
+        } else {
+            return (
+                <Button onClick={handleFollow}>
+                    Follow
+                </Button>
+            )
+        }
+    }
+
+  return (
+    <>
+        {renderFollowButton()}
+    </>
+  )
+}
+
+export default FollowButton;
+
