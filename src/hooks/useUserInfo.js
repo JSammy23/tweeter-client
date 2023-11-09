9a0c633b1b4f3febd7c6a9db7a57a0bf55d9dcfa
Jordan <jordansampson23@gmail.com>
2023-11-09 13:30:40 -0600

    Refactor to take populate parameters.

--- src/hooks/useUserInfo.js
+++ src/hooks/useUserInfo.js
@@ -1,30 +1,31 @@
 import { useState, useEffect } from 'react';
 import { fetchUser } from '../api/users';
 
-const useUserInfo = ({ userId }) => {
+const useUserInfo = ({ userId, fields = '', populate = '' }) => {
     const [userInfo, setUserInfo] = useState(null);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchUserInfo = async () => {
+            if (!userId) return;
+            
             setLoading(true);
             try {
-                // console.log('Fecth useeffect triggered!', userId)
-                const user = await fetchUser(userId);
+                console.log('Fetch useEffect triggered!', userId);
+                const user = await fetchUser(userId, fields, populate);
                 setUserInfo(user);
-                // console.log('User found:', user);
+                console.log('User found:', user);
             } catch (error) {
                 console.error("Error fetching user:", error);
             } finally {
                 setLoading(false);
             }
-        }
+        };
 
-        if (userId) {
-            fetchUserInfo()
-        }
+        fetchUserInfo();
 
-    }, [userId]);
+    }, [userId, fields, populate]);
+
     return { userInfo, loading };
 };
 
