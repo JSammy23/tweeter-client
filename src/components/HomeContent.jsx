dd6f784fd9917b8d2b21d64f2ac392c67d08535d
Jordan <jordansampson23@gmail.com>
2023-12-23 13:53:19 -0600

    Use getHomeTweetQuery hook over tweetfetcher.

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,16 +1,17 @@
-import { fetchHomeTweets } from '../api/tweets';
+import { useGetHomeTweetsQuery } from '../api/tweets';
 import Compose from './Compose';
-import TweetFetcher from './TweetFetcher';
+import TweetList from './TweetList';
 
 import styled from 'styled-components';
 
 
 const HomeContent = () => {
-    
+    const { data: tweets, isLoading, isError } = useGetHomeTweetsQuery({ limit: 50, skip: 0 });
+
   return (
     <>
       <Compose action='tweet' />
-      <TweetFetcher fetchDataFunction={fetchHomeTweets} showType='subscribedTweets' />
+      {isLoading ? <p>Loading...</p> : isError ? <p>Error Loading Tweets</p> : <TweetList tweets={tweets} />}
     </>
   )
 }
