3b3d5d6e721ec88dc4790890c2238178ad8becae
Jordan <jordansampson23@gmail.com>
2023-12-23 14:12:55 -0600

    Add updateUserLikes reducer.

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -12,8 +12,16 @@ export const userSlice = createSlice({
         clearUser: (state) => {
             state.currentUser = null;
         },
+        updateUserLikes: (state, action) => {
+            const { tweetId, isLiked } = action.payload;
+            if (!isLiked) {
+                state.currentUser.likes.push(tweetId);
+            } else {
+                state.currentUser.likes = state.currentUser.likes.filter(id => id !== tweetId);
+            }
+        },
     },
 });
 
-export const { setUser, clearUser } = userSlice.actions;
+export const { setUser, clearUser, updateUserLikes } = userSlice.actions;
 export default userSlice.reducer;
