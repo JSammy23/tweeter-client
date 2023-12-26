278d14b178bad94ff78c35c3b82de32732e076fe
Jordan <jordansampson23@gmail.com>
2023-12-26 13:43:04 -0600

    Refactor to useGetUserTweetsAndLikes query.

--- src/components/ProfileContent.jsx
+++ src/components/ProfileContent.jsx
@@ -3,27 +3,34 @@ import UserProfile from "./UserProfile";
 import FollowList from "./FollowList";
 import useUserInfo from "../hooks/useUserInfo";
 import Loading from "./Loading/Loading";
+import { useGetUserTweetsAndLikesQuery } from "../api";
 
 
 const ProfileContent = () => {
     const { userId } = useParams();
-    const { userInfo, loading } = useUserInfo({
+    const { userInfo } = useUserInfo({
       userId: userId,
       populate: 'following:firstName lastName username profile.profile_picture,followers:firstName lastName username profile'
     });
+    const { data: tweets, isLoading, isError } = useGetUserTweetsAndLikesQuery({ userId, limit: 75, skip: 0 });
+
+    // Combine userTweets and userRetweets
+    const combinedTweets = tweets?.userTweets && tweets?.userRetweets 
+                           ? [...tweets.userTweets, ...tweets.userRetweets]
+                           : [];
     
-    if (loading) {
-        return <Loading />;
+    if (isLoading) {
+      return <Loading />;
     }
 
   return (
     <Routes>
-        <Route path="/" element={<UserProfile user={userInfo} />} />
+        <Route path="/" element={<UserProfile user={userInfo} tweets={combinedTweets} />} />
         <Route path="followers" element={<FollowList user={userInfo} listType='followers' followers={userInfo?.followers} />} />
         <Route path="following" element={<FollowList user={userInfo} listType='following' following={userInfo?.following} />} />
-        <Route path="likes" element={<UserProfile user={userInfo} showLikes={true} />} />
+        <Route path="likes" element={<UserProfile user={userInfo} tweets={tweets.userLikes} />} />
     </Routes>
   )
 }
 
-export default ProfileContent
+export default ProfileContent;
