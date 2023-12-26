e2179a99b56a7a7851f761dfca74cb7448d3ac50
Jordan <jordansampson23@gmail.com>
2023-12-26 13:46:21 -0600

    Clean code.

--- src/api/tweets.js
+++ src/api/tweets.js
@@ -15,6 +15,7 @@ export const tweetsApi = createApi({
         },
     }),
     endpoints: (builder) => ({
+        /****** Fetch Tweets ********/
         getHomeTweets: builder.query({
             query: ({ limit = 50, skip = 0 }) => `home?limit=${limit}&skip=${skip}`,
         }),
@@ -24,7 +25,10 @@ export const tweetsApi = createApi({
         getTweetThread: builder.query({
             query: (tweetId) => `thread/${tweetId}`,
         }),
-        // Add other endpoints
+        getUserTweetsAndLikes: builder.query({
+            query: ({ userId, limit = 100, skip = 0 }) => `profile/${userId}?limit=${limit}&skip=${skip}`,
+        }),
+        /****** Interact with Tweet ******/
         interactWithTweet: builder.mutation({
             query: ({ tweetId, action }) => ({
                 url: `${tweetId}/interact`,
@@ -40,92 +44,9 @@ export const {
     useInteractWithTweetMutation,
     useGetExploreTweetsQuery,
     useGetTweetThreadQuery,
+    useGetUserTweetsAndLikesQuery,
 } = tweetsApi;
 
-/****** Fetch Tweets ********/
-
-// Fetch Tweets for Home content
-// export const fetchHomeTweets = async (limit = 100, skip = 0) => {
-//     const token = localStorage.getItem('token');
-//     const response = await fetch(`${BASE_URL}/home?limit=${limit}&skip=${skip}`, {
-//         method: "GET",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": `${token}`
-//         }
-//     })
-
-//     if (!response.ok) {
-//         const errorData = await response.json();
-//         throw new Error(errorData.message);
-//     }
-
-//     const tweets = await response.json();
-//     console.log(tweets); // Remove before production
-//     return tweets;
-// };
-
-// Fetch Tweets for Explore content
-export const fetchExploreTweets = async (limit = 100, skip = 0) => {
-    const token = localStorage.getItem('token');
-    const response = await fetch(`${BASE_URL}/explore?limit=${limit}&skip=${skip}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `${token}`
-        }
-    })
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-    }
-
-    const tweets = await response.json();
-    console.log(tweets); // Remove before production
-    return tweets;
-};
-
-// Fetch User profile Tweets and Likes
-export const fetchUserTweetsAndLikes = async (userId, limit = 100, skip = 0) => {
-    const token = localStorage.getItem('token');
-    const response = await fetch(`${BASE_URL}/profile/${userId}?limit=${limit}&skip=${skip}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `${token}`
-        }
-    })
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-    }
-
-    const tweets = await response.json();
-    console.log(tweets); // Remove before production
-    return tweets;
-};
-
-// Fetch single Tweet with replies
-export const fetchTweetAndReplies = async (tweetId) => {
-    const token = localStorage.getItem('token');
-    const response = await fetch(`${BASE_URL}/thread/${tweetId}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `${token}`
-        }
-    })
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-    }
-
-    const tweet = await response.json();
-    console.log('Fetched Thread:', tweet); // Remove before production
-    return tweet;
-};
 
 /******** Create Tweets ********/
 
@@ -170,26 +91,3 @@ export const softDeleteTweet = async (tweetId) => {
     console.log(data);
     return data;
 };
-
-// /****** Interact with Tweet ******/
-export const interactWithTweet = async (tweetId, action) => {
-    const token = localStorage.getItem('token');
-    const body = { action: action };
-    const response = await fetch(`${BASE_URL}/${tweetId}/interact`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `${token}`
-        },
-        body: JSON.stringify(body),
-    })
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-    }
-
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
