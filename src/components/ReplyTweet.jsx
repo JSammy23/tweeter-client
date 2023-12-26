48c96ea00a77cd12a89db3d5d97b35aa31e916d7
Jordan <jordansampson23@gmail.com>
2023-12-26 13:43:42 -0600

    Refactor to useGetTweetThreadQuery & RTK state.

--- src/components/ReplyTweet.jsx
+++ src/components/ReplyTweet.jsx
@@ -1,54 +1,32 @@
-import { useState, useEffect } from 'react';
 import StandardTweet from './StandardTweet';
-import MissingTweet from './MissingTweet';
-import { fetchTweetAndReplies } from '../api/tweets';
+import { useGetTweetThreadQuery } from '../api/tweets';
 
 const ReplyTweet = ({ initialTweet }) => {
-    const [thread, setThread] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        const fetchThread = async () => {
-            setIsLoading(true);
-            let currentTweet = initialTweet;
-            let tweets = [currentTweet];
-
-            while (currentTweet.replyTo) {
-                try {
-                    const parentTweet = await fetchTweetAndReplies(currentTweet.replyTo);
-                    tweets.push(parentTweet);
-                    currentTweet = parentTweet;
-                } catch (error) {
-                    console.error("Error fetching tweet:", error);
-                    tweets.push({ missing: true });
-                    break;
-                }
-            }
-
-            // Add initialTweet only if it's not already in the thread
-            if (!tweets.find(t => t._id === initialTweet._id)) {
-                tweets.push(initialTweet);
-            }
-
-            setThread(tweets.reverse());
-            setIsLoading(false);
-        };
-
-        if (initialTweet) {
-            fetchThread();
-        }
-    }, [initialTweet]);
+    const { data: tweetData, isLoading, isError } = useGetTweetThreadQuery(initialTweet._id);
 
     if (isLoading) {
         return <p>Loading...</p>;
     }
 
+    if (isError || !tweetData) {
+        return <p>Error loading tweet thread.</p>;
+    }
+
+    const renderTweetAndReplies = (tweet, isInitialTweet = false) => {
+        return (
+            <div key={tweet._id}>
+                {tweet.replyTo && (
+                    <StandardTweet tweet={tweet.replyTo} isMini />
+                )}
+                <StandardTweet tweet={tweet} isMini={!isInitialTweet} />
+                {tweet.replies && tweet.replies.map(reply => renderTweetAndReplies(reply))}
+            </div>
+        );
+    };
+
     return (
         <div>
-            {thread.map((tweet, index) => tweet.missing ? 
-                <MissingTweet key={index} isMini /> :
-                <StandardTweet key={tweet._id || index} tweet={tweet} isMini={index !== thread.length - 1} />
-            )}
+            {renderTweetAndReplies(tweetData, true)}
         </div>
     );
 };
