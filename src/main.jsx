9f84e9c5a19a6b0992477398db0530721eac22ea
Jordan <jordansampson23@gmail.com>
2023-12-28 10:51:46 -0600

    Remove React userContext & Provider.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import {  BrowserRouter } from 'react-router-dom';
 import theme from './styles/theme.js';
 import { ThemeProvider } from 'styled-components';
-import { UserProvider } from './services/userContext.jsx';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
 
@@ -14,11 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme} >
-        <UserProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </UserProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
