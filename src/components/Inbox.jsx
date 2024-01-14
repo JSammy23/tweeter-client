391a08536678be2ef8f43a42db2eb60365cb1e3c
Jordan <jordansampson23@gmail.com>
2024-01-14 11:25:19 -0600

    Navigate to conversation on click. Styling.

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -1,15 +1,29 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import styled from "styled-components";
 import Typography from '@mui/material/Typography';
+import { TweetDate } from '../styles/tweetStyles';
 
 const ConversationDiv = styled.div`
     display: flex;
     width: 100%;
+    border-bottom: 1px solid ${props => props.theme.colors.secondary};
+    padding: .5em;
+
+    &:hover {
+        color: ${props => props.theme.colors.accent};
+        cursor: pointer;
+    }
 `
 
 const Inbox = ({ conversations }) => {
     const currentUser = useSelector(state => state.user.currentUser);
+    const navigate = useNavigate();
+
+    const handleConversationClick = (conversationId) => {
+        navigate(`/messages/${conversationId}`);
+    };
 
     const renderConversations = () => {
         if (conversations && conversations.length > 0) {
@@ -18,10 +32,13 @@ const Inbox = ({ conversations }) => {
                     .filter(participant => participant._id !== currentUser._id)
                     .map(participant => participant.username).join(', ');
                 return (
-                    <ConversationDiv key={conversation._id}>
+                    <ConversationDiv key={conversation._id} onClick={() => handleConversationClick(conversation._id)}  >
                         <Typography variant="h5">
                             {participantNames}
                         </Typography>
+                        <TweetDate>
+                            {conversation.lastMessageDate ? conversation.lastMessageDate : null}
+                        </TweetDate>
                     </ConversationDiv>
                 );
             });
