8e250068f77118f28dc477b53c80d6d0c3778add
Jordan <jordansampson23@gmail.com>
2024-01-15 19:07:17 -0600

    handle sending message.

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useGetConversationsMessegesQuery } from '../api';
+import { useGetConversationsMessegesQuery, useCreateMessageMutation } from '../api';
 import { useSelector } from 'react-redux';
 import ChatList from './ChatList';
 import TextField from '@mui/material/TextField';
 
 import styled from 'styled-components';
+
+/************ Styling ****************/
 
 const StyledTextField = styled(TextField)`
   width: 100%;
@@ -25,21 +27,51 @@ const ChatListStyle = styled.div`
 const InputContainer = styled.div`
   align-self: center;
   padding: 10px;
-  width: 85%; // Or any other width you prefer
+  width: 85%; 
+
+  @media (max-width: 683px) {
+    margin-bottom: 4em;
+  }
 `;
 
-
+/************ Component ****************/
 
 const Conversation = () => {
     const [skip, setSkip] = useState(0);
     const { conversationId } = useParams();
-    const { data, isLoading, isError } = useGetConversationsMessegesQuery({ limit: 25, skip: skip, conversationId });
+    const [messageText, setMessageText] = useState('');
+    const [createMessage, { isLoading: isCreatingMessage }] = useCreateMessageMutation();
+    const { data, isLoading, isError, refetch } = useGetConversationsMessegesQuery({ limit: 25, skip: skip, conversationId });
     const currentUser = useSelector(state => state.user.currentUser);
 
 
     const handleLoadMore = () => {
         const newSkip = skip + 25;
         setSkip(newSkip);
+    };
+
+    const handleSendMessage = async () => {
+        if (messageText.trim() === '') return;
+
+        try {
+            await createMessage({
+                conversationId,
+                senderId: currentUser._id,
+                text: messageText
+            }).unwrap();
+    
+            setMessageText('');
+            refetch();
+        } catch (error) {
+            console.error('Failed to send message:', error);
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault(); // Prevent default to avoid newline in case of multiline TextField
+            handleSendMessage();
+        }
     };
 
     // Log the messages for debugging purposes
@@ -84,6 +116,9 @@ const Conversation = () => {
               maxRows={5}
               variant='outlined'
               placeholder='Type a message...'
+              value={messageText}
+              onChange={(e) => setMessageText(e.target.value)}
+              onKeyDown={handleKeyDown}
             />
           </InputContainer>
         </ChatContainer>
