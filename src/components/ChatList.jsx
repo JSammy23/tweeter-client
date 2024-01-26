a3c82e5812d66b6732d7f47ed0d1b1642ccb1d76
Jordan <jordansampson23@gmail.com>
2024-01-26 14:17:37 -0600

    Add bottomRef for scrolling to bottom of chatList on mount.

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import ChatBubble from "./ChatBubble";
 
 import styled from "styled-components";
@@ -6,14 +7,27 @@ const ChatContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start; // Default alignment
+  overflow-y: auto;
 `;
 
 const ChatList = ({ messages }) => {
+    const bottomRef = useRef(null);
+
+    useEffect(() => {
+        console.log('Scrolling to bottom');
+        const timer = setTimeout(() => {
+            bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+
+        return () => clearTimeout(timer);
+    }, [messages]);
+
   return (
     <ChatContainer>
         {messages.map((message, index) => (
             <ChatBubble key={index} message={message}  />
         ))}
+        <div ref={bottomRef} />
     </ChatContainer>
   )
 }
