ccc86b0acaed7a9e1c95a6d3f93295d222e2fe61
Jordan Sampson <jordansampson23@gmail.com>
2024-03-05 21:08:20 -0600

    Install react query, add queryClientProvider.

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,24 @@ import theme from './styles/theme.js';
 import { ThemeProvider } from 'styled-components';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import './index.css';
+
+const queryClient = new QueryClient();
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme} >
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient} >
+        <ThemeProvider theme={theme} >
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 )
